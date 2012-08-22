3448789786dd37873381bf80ebd27941ae834981
jose miguel <miguel.gamazo@galiclick.com>
2012-08-22 16:45:19 +0200

    Corrigiendo errores

--- app/overrides/admin_decorator.rb
+++ app/overrides/admin_decorator.rb
@@ -2,5 +2,5 @@ Deface::Override.new(
   :name => "add_admin_menu",
   :virtual_path => "spree/layouts/admin",
   :insert_bottom => "[data-hook='admin_tabs']",
-  :text => "<%= tab :cycle_images, :label => 'Cycle Images' %>"
+  :text => "<%= tab :cycle_images, :label => 'Gestion Banner' %>"
 )
