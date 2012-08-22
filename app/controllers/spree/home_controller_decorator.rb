7963a3f1a84bba72e422db2aa0f620d5db686662
jose miguel <miguel.gamazo@galiclick.com>
2012-08-22 17:47:09 +0200

    Correcion de errores

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -1,8 +1,12 @@
 Spree::HomeController.class_eval do
+  helper 'spree/products'
+  respond_to :html
+
   def index
     @searcher = Spree::Config.searcher_class.new(:in_offert => true)
     @products = @searcher.retrieve_products
-    respond_with(@products)
     @cycle_images = Spree::CycleImage.all
+    respond_with(@products)
   end
+
 end
