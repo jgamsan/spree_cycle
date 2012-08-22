298aded22ca80e4e4b101d66e010ec603c1973f8
jose miguel <miguel.gamazo@galiclick.com>
2012-08-22 16:49:34 +0200

    Corrigiendo errores

--- app/models/spree/cycle_image.rb
+++ app/models/spree/cycle_image.rb
@@ -6,6 +6,7 @@ class Spree::CycleImage < ActiveRecord::Base
   acts_as_list
 
   default_scope :order => 'position ASC'
+  attr_accessible :image, :description
 
   validates :description, :length => { :minimum => 2, :maximum => 255 }, :allow_blank => true
   validates :link, :length => { :minimum => 2, :maximum => 255 }, :allow_blank => true
