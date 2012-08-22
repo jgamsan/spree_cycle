16c3f692e10ef6c33e9fd5f3e1eb91671e1e59ce
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-08-22 13:37:33 +0200

    Mejoras en home

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -1,6 +1,6 @@
 Spree::HomeController.class_eval do
   def index
-      @searcher = Spree::Config.searcher_class.new(params)
+      @searcher = Spree::Config.searcher_class.new(:in_offert => true)
       @products = @searcher.retrieve_products
       respond_with(@products)
       @cycle_images = Spree::CycleImage.all
