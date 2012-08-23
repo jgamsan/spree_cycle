250337ec61fe96dd2f846b8190daccea44e92399
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-08-23 10:29:00 +0200

    Error en index

--- app/controllers/spree/cycle_images_controller.rb
+++ app/controllers/spree/cycle_images_controller.rb
@@ -1,5 +1,8 @@
 class Spree::CycleImagesController < Spree::BaseController
   def index
+    @searcher = Spree::Config.searcher_class.new(:in_offert => true)
+    @products = @searcher.retrieve_products
+    respond_with(@products)
     @cycle_images = Spree::CycleImage.all
   end
 end
