21ac28a8cf8631a18997b2205ec9933712815eca
jose miguel <miguel.gamazo@galiclick.com>
2012-08-23 17:43:29 +0200

    Prueba con paginado

--- app/controllers/spree/cycle_images_controller.rb
+++ app/controllers/spree/cycle_images_controller.rb
@@ -6,7 +6,7 @@ class Spree::CycleImagesController < Spree::BaseController
   def index
     #@searcher = Spree::Config.searcher_class.new(:in_offert => true)
     #@products = @searcher.retrieve_products
-    @products = Spree::Product.in_offert(true).page params[:page]
+    @products = Spree::Product.in_offert(true).page(params[:page]).per(12)
     #respond_with(@products)
     @cycle_images = Spree::CycleImage.all
   end
