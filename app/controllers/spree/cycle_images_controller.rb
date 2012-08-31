ffbc00575ae054b819fd35246db401f66e708fe5
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-08-31 10:45:24 +0200

    Updated gemspec

--- app/controllers/spree/cycle_images_controller.rb
+++ app/controllers/spree/cycle_images_controller.rb
@@ -6,7 +6,6 @@ class Spree::CycleImagesController < Spree::BaseController
   def index
     @searcher = Spree::Config.searcher_class.new(:in_offert => true)
     @products = @searcher.retrieve_products
-    #respond_with(@products)
     @cycle_images = Spree::CycleImage.all
   end
 
