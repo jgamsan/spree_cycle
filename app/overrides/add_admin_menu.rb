c71d149d139053750c3e1fc7c0c1704c81bc7b93
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2012-12-06 09:58:33 +0100

    mas mejoras

--- app/overrides/add_admin_menu.rb
+++ app/overrides/add_admin_menu.rb
@@ -2,5 +2,5 @@ Deface::Override.new(
   :name => "add_admin_menu",
   :virtual_path => "spree/layouts/admin",
   :insert_bottom => "[data-hook='admin_tabs']",
-  :text => "<%= tab :cycle_images, :label => 'Gestion Banner' %>"
+  :text => "<%= tab :cycle_images, :label => 'Gestion Banner', :icon => 'icon-facetime-video' %>"
 )
